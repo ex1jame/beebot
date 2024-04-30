2cb1da551f4ba847816afed010ecca502a3bbab6
Temirlan Baysynov <tima.baysynov.2017@gmail.com>
2024-04-30 14:51:45 +0600

    next step

--- app/handlers.py
+++ app/handlers.py
@@ -1,11 +1,20 @@
+import asyncio
+
 from aiogram import Router, F, types
 from aiogram.filters import CommandStart
-from aiogram.types import Message, CallbackQuery
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import StatesGroup, State
+from aiogram.types import Message, CallbackQuery, InputFile
 
 import app.keyboards as kb
+from app.utils.states import states_texts, ConfirmationState, add_states_texts
 
 router = Router()
 
+
+@router.message(F.photo)
+async def get_photo(message: Message, state: FSMContext):
+    await message.answer(f"id{message.photo[-1].file_id}")
 
 @router.message(CommandStart())
 async def cmd_start(message: Message):
@@ -45,7 +54,7 @@ async def get_docs(callback: CallbackQuery):
 @router.callback_query(F.data == 'help')
 async def get_help(callback: CallbackQuery):
     await callback.answer()
-    await callback.message.reply("С чем именно мне вам помочь?",reply_markup=kb.help)
+    await callback.message.reply("С чем именно мне вам помочь?", reply_markup=kb.help)
 
 
 @router.callback_query(F.data == 'instruction_one')
@@ -53,19 +62,67 @@ async def get_instruction_one(callback: CallbackQuery):
     await callback.answer()
     await callback.message.answer("Справку можно заказать через терминал или в гос.портале: portal.tunduk.kg \n"
                                   "После заявки справка выдается в течении от 1-3 дней по адресу:"
-                                  "Курманжан Датка, 115. Тел. (0312) 369-417, (0312) 369-418")
+                                  "Курманжан Датка, 115. Тел. (0312) 369-417, (0312) 369-418", reply_markup=kb.step_one)
 
 
 @router.callback_query(F.data == 'instruction_two')
 async def get_instruction_two(callback: CallbackQuery):
     await callback.answer()
     await callback.message.answer("Медицинскую справку можно получить в поликлинике по "
-                                  "месту прописки или в любом платном медицинском")
+                                  "месту прописки или в любом платном медицинском", reply_markup=kb.step_one)
 
 
 @router.callback_query(F.data == 'instruction_third')
-async def get_instruction_two(callback: CallbackQuery):
+async def get_instruction_third(callback: CallbackQuery):
     await callback.answer()
     await callback.message.answer("Для получения справок необходимо подойти к единую регистратуру в "
                                   "Республиканском центре психиатрии и наркологии. Адрес: Байтик Баатыра, 1а."
-                                  " Либо можно заказать в гос. портале: portal.tunduk.kg")
+                                  " Либо можно заказать в гос. портале: portal.tunduk.kg", reply_markup=kb.step_one)
+
+
+@router.message(F.text == 'Дальше')
+async def next_step(message: Message, state: FSMContext):
+    current_state = await state.get_state()
+    if current_state is None:
+        await state.set_state(1)  # Установка начального состояния 1
+        await message.answer(states_texts[1])
+        await message.answer(add_states_texts[1])
+
+    else:
+        current_state = int(current_state)  # Преобразование текущего состояния в число
+        next_state = current_state + 1
+        await state.set_state(next_state)  # Установка следующего состояния
+
+        if next_state == 2:
+            await message.answer(states_texts[next_state], reply_markup=kb.yes_or_no)
+
+        elif next_state == 3:
+            await message.answer(states_texts[3])
+            await message.answer(add_states_texts[2])
+        elif next_state == 4:
+            # Отправляем фотографию и текст
+            await message.answer(states_texts[4])
+
+            await message.answer_photo(photo='AgACAgIAAxkBAAICtGYvuip5z8PgMJHOQamly42gVGPrAAII2zEbRCyASQKbWDGvFjGTAQADAgADeAADNAQ',
+                                       caption=add_states_texts[3])
+        elif next_state == 5:
+            await message.answer(states_texts[5])
+            await message.answer(add_states_texts[5])
+
+        elif next_state in states_texts:
+            await message.answer(states_texts[next_state])
+        else:
+            await message.answer("Больше нет текстов для отображения.")
+
+
+@router.callback_query(F.data == 'yes_docs')
+async def get_yes_docs(callback: CallbackQuery, state: FSMContext):
+    await callback.answer()
+    await callback.message.answer("Отлично!")
+
+
+@router.callback_query(F.data == 'no_docs')
+async def get_yes_docs(callback: CallbackQuery, state: FSMContext):
+    await callback.answer()
+    await callback.message.answer("Поторопись,чтобы мы успели оформить тебя")
+
